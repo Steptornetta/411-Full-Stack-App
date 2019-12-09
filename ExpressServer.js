d6882d8273f27f374554e74d83cd9795da9a657b
Steve Tornetta <steptornetta@yahoo.com>
2019-12-09 11:01:16 -0500

    Added Story and HTML

--- ExpressServer.js
+++ ExpressServer.js
@@ -6,35 +6,58 @@ var cors = require('cors');
 
 app.use(cors());
 
-let MadLib = [];
+// http://www.redkid.net/cgi-bin/madlibs/drivingtest.pl
+let MadLib = [
+"Driving a car can be fun if you follow this BLANK.advice: \
+When approaching a BLANK.on the right, always blow your BLANK.\
+Before making a BLANK.turn, always stick your BLANK.out of the window.\
+Every 2000 miles, have your BLANK.inspected and your BLANK.checked.\
+When approaching a school, watch out for BLANK.BLANK.\
+Above all, drive BLANK. The BLANK.you save may be your own!"
+
+];
+
 let MadLibdata = [];
+
+
 /*
-GET API request. Sends the reservation list to the client.
+GET API request. Sends the madlib to the client.
 */
 app.get('/madlib', (req, res) => 
 {
 	console.log("GET MADLIB REQUEST");
 	MadLibdata = getMadLib();
-	MadLib = JSON.parse(MadLibdata);
-	console.log(MadLibdata);
 	res.send(MadLibdata);
 });
 
 
-// http://www.redkid.net/cgi-bin/madlibs/drivingtest.pl
+
+app.put('/madlib/update'. (req, res) =>
+{
+	console.log("PUT REQUEST");
+
+});
+
 function getMadLib()
 {
-	if(fs.existsSync('story.txt'))
+	if(fs.existsSync("story.txt"))
 	{
+		//fs.writeFile(, data, options, flag?, mode?, callback)
 		MadLibdata = fs.readFileSync('story.txt');
-		console.log(MadLibdata);
-		return MadLibdata;
+		return MadLib;
 	}
 	else
 	{
-		console.log("There is no story file.")
-		return "No story Error";
+		fs.writeFileSync('story.txt', MadLib, err =>
+		{
+			if (err) throw err;
+			console.log('Saved File')
+		});
+
+		return MadLib;
+
 	}
 }
+
 
 app.listen(port, () => console.log(`MadLib Server on: ${port}!`));
